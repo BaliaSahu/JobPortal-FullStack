0781ed173ec78970abef8c007603c0168faa8fe7
Balia Sahu <baliasahu7382@gmail.com>
2026-01-15 22:54:16 +0530

    Update Home component layout and add Recruiter link

--- frontend/user-app/src/pages/Homes/Home.jsx
+++ frontend/user-app/src/pages/Homes/Home.jsx
@@ -3,8 +3,17 @@ import Trusted from "../../components/Trusted/Trusted.jsx";
 
 const Home=()=>{
     return(
-        <div>
+        <div  className="d-flex justify-center " style={{ flexDirection: "column", alignItems: "center" }}>
             <Header></Header>
+            <div><a
+                href="https://jobcrackr.netlify.app"
+                className="btn btn-warning fw-semibold"
+                target="_blank"
+                rel="noopener noreferrer"
+            >
+                Recruiter
+            </a>
+            </div>
             <Trusted></Trusted>
         </div>
     )
